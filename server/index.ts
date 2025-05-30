81265bd06a8d556f3ca0776b3fb4472894cf4e30
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2025-05-30 19:35:14 +0530

    feat: update npm scripts

--- server/index.ts
+++ server/index.ts
@@ -57,9 +57,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
+  // Always use process.env.PORT in cloud platforms like Railway
+  // Railway exposes only the port it assigns (not necessarily 5000)
   const port = process.env.PORT || 5000;
   server.listen({
     port,
